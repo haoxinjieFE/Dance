62f8a05f01332832e8b482c3fac782ee45d8971b
haoxinjie <haoxinjie_fe@126.com>
2019-05-30 16:44:00 +0800

    update

--- serve.js
+++
@@ -1,9 +0,0 @@
-const connect = require('connect');
-const serveStatic = require('serve-static');
-const { join } = require('path');
-
-connect()
-  .use(serveStatic(join(__dirname, 'dist')))
-  .listen(8080, () => {
-    console.log('Server running on 8080...');
-  });
